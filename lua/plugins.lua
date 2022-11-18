dfcae1989325ca11c24a80ff9f0a158e93094c4b
Thomas Crescenzi <trescenzi@gmail.com>
2022-11-17 21:01:47 -0500

    switch to catppuccin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,23 +8,49 @@ paq({
   {"ms-jpq/coq_nvim", branch="coq"};
   "nvim-treesitter/nvim-treesitter";
   "sainnhe/everforest";
-  "rebelot/kanagawa.nvim";
   "Olical/conjure";
   'Olical/aniseed';
   'tomlion/vim-solidity';
   'jparise/vim-graphql';
   'sam4llis/nvim-tundra';
   'tpope/vim-repeat';
-  'ggandor/leap.nvim';
+  'catppuccin/nvim';
 })
 
 function setupPlugins()
   -- needs to be called before we try to require coq
   vim.g.coq_settings={auto_start='shut-up'}
-  vim.g.everforest_material_enable_italic=1
-  vim.g.everforest_background='soft'
-  vim.opt.background=os.getenv('term_background') or 'dark'
-  vim.cmd('colorscheme everforest');
+  -- vim.g.everforest_material_enable_italic=1
+  -- vim.g.everforest_background='soft'
+  -- require('colors');
+  vim.g.background=os.getenv('term_background') or 'dark'
+  require("catppuccin").setup({
+    -- latte, frappe, macchiato, mocha
+    flavour = "frappe",
+    background = {
+        light = "latte",
+        dark = "macchiato",
+    },
+    integrations = {
+      treesitter = true,
+      native_lsp = {
+        enabled = true,
+        virtual_text = {
+          errors = { "italic" },
+          hints = { "italic" },
+          warnings = { "italic" },
+          information = { "italic" },
+        },
+        underlines = {
+          errors = { "underline" },
+          hints = { "underline" },
+          warnings = { "underline" },
+          information = { "underline" },
+        },
+      },
+    }
+  });
+  vim.cmd('colorscheme catppuccin');
   require('disable_defaults')
   require('lsp')
   require('treesitter')
