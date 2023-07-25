d77180e19792e57f8a17ca886904baac93995e2c
Thomas Crescenzi <trescenzi@gmail.com>
2023-07-24 19:53:21 -0500

    update colors and remove unnused plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -7,10 +7,6 @@ paq({
   "neovim/nvim-lspconfig";
   {"ms-jpq/coq_nvim", branch="coq"};
   "nvim-treesitter/nvim-treesitter";
-  "Olical/conjure";
-  'Olical/aniseed';
-  'catppuccin/nvim';
-  "tomlion/vim-solidity";
   "jparise/vim-graphql";
   "f-person/git-blame.nvim";
   "ellisonleao/gruvbox.nvim";
@@ -19,8 +15,6 @@ paq({
 function setupPlugins()
   -- needs to be called before we try to require coq
   vim.g.coq_settings={auto_start='shut-up'}
-  -- vim.g.everforest_material_enable_italic=1
-  -- vim.g.everforest_background='soft'
   require('colors');
   vim.g.background=os.getenv('term_background') or 'dark'
   vim.cmd('colorscheme gruvbox');
